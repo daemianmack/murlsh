8fe4ccf03340b3b8fee7adc761cda37ec58e61f9
Matthew M. Boedicker <matthewm@boedicker.org>
2009-08-03 18:26:04 -0400

    add google.ca

--- lib/murlsh/referrer.rb
+++ lib/murlsh/referrer.rb
@@ -35,7 +35,7 @@ module Murlsh
     end
 
     Qmap = {
-      /^www\.google\.(bs|com|cz|dk|es|nl)(\/m)?\/search$/ => 'q',
+      /^www\.google\.(bs|ca|com|cz|dk|es|nl)(\/m)?\/search$/ => 'q',
       /^www\.bing\.com\/search$/ => 'q',
     }
 

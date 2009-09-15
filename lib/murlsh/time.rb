52bc60e205ff09a439541be5b15c70e2d22121b1
Matthew M. Boedicker <matthewm@boedicker.org>
2009-09-14 23:27:50 -0400

    show day of week for 5 - 7 days ago

--- lib/murlsh/time.rb
+++ lib/murlsh/time.rb
@@ -9,7 +9,8 @@ class Time
        when 0 then 'today'
        when 1 then 'yesterday'
        when (2..4) then "#{days_ago} days ago"
-       when (5..180) then strftime('%e %b').strip
+       when (5..7) then strftime('%a %e %b')
+       when (8..180) then strftime('%e %b').strip
        else strftime('%e %b %Y').strip
     end
   end

5ded260724a74425818885b296bd8fdf6951d44b
Matthew M. Boedicker <matthewm@boedicker.org>
2009-10-27 22:21:19 -0400

    add rdoc

--- lib/murlsh/plugin.rb
+++ lib/murlsh/plugin.rb
@@ -1,11 +1,22 @@
 module Murlsh
 
+  # Superclass for plugins. How plugins are registered.
+  # 
+  # Hooks:
+  #
+  # add_pre - called before a new url is saved
+  #   run arguments (url, config hash)
+  # add_post - called after a new url is saved
+  #   run arguments (config hash)
   class Plugin
 
+    # Called when a plugin class inherits from this class (the way plugins
+    # are registered).
     def self.inherited(child)
       registered << child
     end
 
+    # Get registered plugins by hook (add_pre, add_post, etc.)
     def self.hooks(name)
       matches = registered.select { |p| p::Hook == name }
 

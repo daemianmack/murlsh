2b92b2fef2081fd7653584d2a3b2f7e4fd90494f
Matthew M. Boedicker <matthewm@boedicker.org>
2010-03-02 20:41:40 -0500

    convert to single quotes

--- test/uri_test.rb
+++ test/uri_test.rb
@@ -4,11 +4,11 @@ require 'murlsh'
 
 describe URI do
 
-  it "should have its domain set to the domain of its URI if it is a valid HTTP URI" do
+  it 'should have its domain set to the domain of its URI if it is a valid HTTP URI' do
     URI('http://foo.com/').domain.should == 'foo.com'
   end
 
-  it "should have its domain set nil if it is no a valid HTTP URI" do
+  it 'should have its domain set nil if it is no a valid HTTP URI' do
     URI('foo').domain.should be_nil
     URI('http://foo.com.').domain.should be_nil
   end

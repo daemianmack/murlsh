80004f5f6120dee45ad1c73cb44aabdcb2ce8f0f
Matthew M. Boedicker <matthewm@boedicker.org>
2010-04-04 01:05:00 -0400

    regex based dispatch

--- lib/murlsh/dispatch.rb
+++ lib/murlsh/dispatch.rb
@@ -10,36 +10,40 @@ module Murlsh
   # Dispatch requests.
   class Dispatch
 
-    # Set up config hash and database connection.
+    # Set up database connection and dispatch table.
     def initialize(config)
       @config = config
-      @url_root = URI(@config.fetch('root_url')).path
 
       ActiveRecord::Base.establish_connection(
         :adapter => 'sqlite3', :database => @config.fetch('db_file'))
 
-      @db = ActiveRecord::Base.connection.instance_variable_get(:@connection)
+      db = ActiveRecord::Base.connection.instance_variable_get(:@connection)
 
-      @url_server = Murlsh::UrlServer.new(@config, @db)
-      @flickr_server = Murlsh::FlickrServer.new(@config)
+      url_server = Murlsh::UrlServer.new(@config, db)
+      flickr_server = Murlsh::FlickrServer.new(@config)
+
+      root_path = URI(@config.fetch('root_url')).path
+
+      @dispatch = [
+        [/^GET #{root_path}(url)?$/, url_server.method(:get)],
+        [/^POST #{root_path}(url)?$/, url_server.method(:post)],
+        [/^GET \/flickr$/, flickr_server.method(:get)],
+      ]
+    end
+
+    # Figure out which method will handle request.
+    def dispatch(req)
+      method_match = @dispatch.find do |rule|
+        rule[0].match("#{req.request_method} #{req.path}")
+      end
+
+      method_match ? method_match[1] : self.method(:not_found)
     end
 
     # Rack call.
     def call(env)
-      url_url = "#{@url_root}url"
-
-      dispatch = {
-        ['GET', @url_root] => @url_server.method(:get),
-        ['POST', @url_root] => @url_server.method(:post),
-        ['GET', url_url] => @url_server.method(:get),
-        ['POST', url_url] => @url_server.method(:post),
-        ['GET', '/flickr'] => @flickr_server.method(:get),
-      }
-      dispatch.default = self.method(:not_found)
-
       req = Rack::Request.new(env)
-
-      dispatch[[req.request_method, req.path]].call(req).finish
+      dispatch(req).call(req).finish
     end
 
     # Called if the request is not found.

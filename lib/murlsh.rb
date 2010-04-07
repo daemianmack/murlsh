a0b65eb8ce1c2663ad4fe301bd4e3733cc32c7cd
Matthew M. Boedicker <matthewm@boedicker.org>
2010-04-06 20:12:09 -0400

    require all .rb files in murlsh instead of listing explicitly

--- lib/murlsh.rb
+++ lib/murlsh.rb
@@ -1,21 +1,4 @@
-require 'murlsh/doc'
-require 'murlsh/uri'
-require 'murlsh/auth'
-require 'murlsh/dispatch'
-require 'murlsh/failproof'
-require 'murlsh/flickr_server'
-require 'murlsh/openlock'
-require 'murlsh/plugin'
-require 'murlsh/sqlite3_adapter'
-require 'murlsh/time_ago'
-require 'murlsh/url_server'
-require 'murlsh/url'
-require 'murlsh/uri_ask'
-require 'murlsh/xhtml_response'
-require 'murlsh/yaml_ordered_hash'
-
-# requiring builder before active_record blows up
-require 'murlsh/atom_feed'
-require 'murlsh/markup'
-require 'murlsh/url_body'
-
+Dir.glob(File.join(File.dirname(__FILE__), 'murlsh', '*.rb')).
+  map { |f| File.join('murlsh', File.basename(f, '.rb')) }.
+  sort.
+  each { |m| require m }

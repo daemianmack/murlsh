2ec5e12f96aa17f420b458c51152560cc1dc0e3b
Matthew M. Boedicker <matthewm@boedicker.org>
2010-04-15 00:10:15 -0400

    better titles for github project pages

---
+++ plugins/add_pre_60_github_title.rb
@@ -0,0 +1,22 @@
+%w{
+murlsh
+}.each { |m| require m }
+
+module Murlsh
+
+  # Github project page titles are not very descriptive so add meta description
+  # to title.
+  class AddPre60GithubTitle < Plugin
+
+    Hook = 'add_pre'
+
+    def self.run(url, config)
+      if url.url[%r{http://github.com/\w+/\w+}]
+        ask = URI(url.url).extend(Murlsh::UriAsk)
+        url.title << " - #{ask.description}" if !ask.description.empty?
+      end
+    end
+
+  end
+
+end

40f9f35e05cd815e1c46234d7fe748dfe8cacb42
Matthew M. Boedicker <matthewm@boedicker.org>
2010-10-06 21:05:58 -0400

    do not show file size for any content types

---
+++ plugins/url_display_add_55_content_type.rb
@@ -0,0 +1,23 @@
+module Murlsh
+
+  # warn of content types that launch external apps
+  class UrlDisplayAdd55ContentType < Plugin
+
+    @hook = 'url_display_add'
+
+    # warn of content types that launch external apps
+    def self.run(markup, url, config)
+      content_type_display = case url.content_type
+        when 'application/pdf' then 'pdf'
+        when 'audio/mpeg' then 'mp3'
+        else ''
+      end
+
+      unless content_type_display.empty?
+        markup.span(" (#{content_type_display})", :class => 'content-type')
+      end
+    end
+
+  end
+
+end

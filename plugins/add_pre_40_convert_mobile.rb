bda6fbc7747eda6be3db931c05eb068c4ef587e4
Matthew M. Boedicker <matthewm@boedicker.org>
2010-10-20 20:38:12 -0400

    convert mobile device urls into their non-mobile equivalents

---
+++ plugins/add_pre_40_convert_mobile.rb
@@ -0,0 +1,24 @@
+%w{
+murlsh
+}.each { |m| require m }
+
+module Murlsh
+
+
+  # Convert urls specifically for use on mobile devices into their non-mobile
+  # equivalents.
+  class AddPre40ConvertMobile < Plugin
+
+    @hook = 'add_pre'
+
+    WikipediaRe = %r{(http://[a-z]+\.)m\.(wikipedia\.org/.*)}
+
+    def self.run(url, config)
+      if match = WikipediaRe.match(url.url)
+        url.url = "#{match[1]}#{match[2]}"
+      end
+    end
+
+  end
+
+end

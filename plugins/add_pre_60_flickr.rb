0849605bc544c53226de321f6561cb4edd794e37
Matthew M. Boedicker <matthewm@boedicker.org>
2010-10-28 21:44:01 -0400

    plugin to get flickr thumbnail and title

---
+++ plugins/add_pre_60_flickr.rb
@@ -0,0 +1,30 @@
+%w{
+flickraw
+
+murlsh
+}.each { |m| require m }
+
+module Murlsh
+
+  # Set the title and thumbnail url of Flickr photos.
+  class AddPre60Flickr < Plugin
+
+    @hook = 'add_pre'
+
+    FlickrRe = %r{^http://(?:www\.)?flickr\.com/photos/[@\w\-]+?/([\d]+)}i
+
+    def self.run(url, config)
+      if config['flickr_api_key'] and not config['flickr_api_key'].empty?
+        if photo_id = url.url[FlickrRe, 1]
+          FlickRaw.api_key = config['flickr_api_key']
+          info = flickr.photos.getInfo(:photo_id => photo_id)
+
+          url.title = "#{info.title} by #{info.owner.username}"
+          url.thumbnail_url = FlickRaw.url_s(info)
+        end
+      end
+    end
+
+  end
+
+end

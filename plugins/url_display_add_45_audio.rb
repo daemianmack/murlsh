8cc4f3022822c5669ab68881029e25204d061327
Matthew M. Boedicker <matthewm@boedicker.org>
2010-11-13 21:07:12 -0500

    use html5 audio instead of flash player for mp3 and ogg urls

---
+++ plugins/url_display_add_45_audio.rb
@@ -0,0 +1,27 @@
+require 'murlsh'
+
+module Murlsh
+
+  # Add HTML5 audio tag to audio urls.
+  class UrlDisplayAdd45Audio < Plugin
+
+    @hook = 'url_display_add'
+
+    AudioContentTypes = %w{
+      application/ogg
+      audio/mpeg
+      audio/ogg
+      }
+
+    def self.run(markup, url, config)
+      if AudioContentTypes.include?(url.content_type)
+        markup.audio(
+          :controls => 'controls',
+          :preload => 'none',
+          :src => url.url)
+      end
+    end
+
+  end
+
+end

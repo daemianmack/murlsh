80779f48887ba81c6a55d304d37e6d4ce3edb08a
Matthew M. Boedicker <matthewm@boedicker.org>
2010-12-12 22:08:10 -0500

    plugin to get slideshare thumbnails

---
+++ plugins/add_pre_60_slideshare.rb
@@ -0,0 +1,38 @@
+require 'cgi'
+
+require 'murlsh'
+
+module Murlsh
+
+  # Set the thumbnail url for slideshare presentations.
+  class AddPre60Slideshare < Plugin
+
+    @hook = 'add_pre'
+
+    SlideshareRe = %r{^http://www\.slideshare\.net/[0-9a-z-]+/[0-9a-z-]+$}i
+    StorageDir = File.join(File.dirname(__FILE__), '..', 'public', 'img',
+      'thumb')
+
+    def self.run(url, config)
+      if url.url[SlideshareRe]
+        ask = URI(url.url).extend(Murlsh::UriAsk)
+        ask.doc.xpath_search("//meta[@rel='media:thumbnail']") do |node|
+          if node and node['href']
+            Murlsh::failproof do
+              thumb_storage = Murlsh::ImgStore.new(StorageDir,
+                :user_agent => config['user_agent'])
+
+              stored_filename = thumb_storage.store_url(node['href']) do |i|
+                max_side = config.fetch('thumbnail_max_side', 90)
+                i.extend(Murlsh::ImageList).resize_down!(max_side)
+              end
+              url.thumbnail_url = "img/thumb/#{CGI.escape(stored_filename)}"
+            end
+          end
+        end
+      end
+    end
+
+  end
+
+end

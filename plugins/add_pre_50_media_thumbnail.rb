2c990d5014fcd8bc3e6f2e81e8f48c77dcf6f254
Matthew M. Boedicker <matthewm@boedicker.org>
2010-12-12 23:14:26 -0500

    generalize slideshare plugin to any page that meta media:thumbnail

---
+++ plugins/add_pre_50_media_thumbnail.rb
@@ -0,0 +1,34 @@
+require 'cgi'
+
+require 'murlsh'
+
+module Murlsh
+
+  # If document has <meta rel="media:thumbnail"> use it as the thumbnail.
+  class AddPre50MediaThumbnail < Plugin
+
+    @hook = 'add_pre'
+
+    StorageDir = File.join(File.dirname(__FILE__), '..', 'public', 'img',
+      'thumb')
+
+    def self.run(url, config)
+      url.ask.doc.xpath_search("//meta[@rel='media:thumbnail']") do |node|
+        if node and node['href']
+          Murlsh::failproof do
+            thumb_storage = Murlsh::ImgStore.new(StorageDir,
+              :user_agent => config['user_agent'])
+
+            stored_filename = thumb_storage.store_url(node['href']) do |i|
+              max_side = config.fetch('thumbnail_max_side', 90)
+              i.extend(Murlsh::ImageList).resize_down!(max_side)
+            end
+            url.thumbnail_url = "img/thumb/#{CGI.escape(stored_filename)}"
+          end
+        end
+      end
+    end
+
+  end
+
+end

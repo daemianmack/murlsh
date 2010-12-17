217fc1bbebb4b15d509f22e4bc1cf0f43d99bc11
Matthew M. Boedicker <matthewm@boedicker.org>
2010-12-16 20:12:36 -0500

    add plugin to unajax twitter urls

---
+++ plugins/add_pre_41_unajax_twitter.rb
@@ -0,0 +1,16 @@
+require 'murlsh'
+
+module Murlsh
+
+  # Convert Ajax friendly Twitter urls (with #!) into usable urls.
+  class AddPre41UnajaxTwitter < Plugin
+
+    @hook = 'add_pre'
+
+    TwitterAjaxRe = %r{^(https?://twitter\.com/)#!/}i
+
+    def self.run(url, config); url.url.sub!(TwitterAjaxRe, '\1'); end
+
+  end
+
+end

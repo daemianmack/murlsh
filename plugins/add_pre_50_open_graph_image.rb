0f6212ca654c9ad330d975dac6e7d052cad3bb2a
Matthew M. Boedicker <matthewm@boedicker.org>
2011-01-14 20:29:11 -0500

    handle youtube og:image urls with no scheme

--- plugins/add_pre_50_open_graph_image.rb
+++ plugins/add_pre_50_open_graph_image.rb
@@ -1,4 +1,5 @@
 require 'cgi'
+require 'uri'
 
 require 'murlsh'
 
@@ -16,11 +17,16 @@ module Murlsh
       if not url.thumbnail_url and url.ask.doc
         url.ask.doc.xpath_search("//meta[@property='og:image']") do |node|
           if node and not node['content'].to_s.empty?
+            og_image_url = node['content']
             Murlsh::failproof do
+              # youtube leaves out scheme: for some reason
+              if og_image_url[%r{^//}]
+                og_image_url = "#{URI(url.url).scheme}:#{og_image_url}"
+              end
               thumb_storage = Murlsh::ImgStore.new(StorageDir,
                 :user_agent => config['user_agent'])
 
-              stored_filename = thumb_storage.store_url(node['content']) do |i|
+              stored_filename = thumb_storage.store_url(og_image_url) do |i|
                 max_side = config.fetch('thumbnail_max_side', 90)
                 i.extend(Murlsh::ImageList).resize_down!(max_side)
               end

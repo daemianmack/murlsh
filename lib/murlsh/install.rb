c1e4b27747fbe581b203ac67b17c6a62f2ba462b
Matthew M. Boedicker <matthewm@boedicker.org>
2011-01-30 16:22:24 -0500

    move install code out of script into module

---
+++ lib/murlsh/install.rb
@@ -0,0 +1,28 @@
+require 'fileutils'
+
+require 'murlsh'
+
+module Murlsh
+
+  MurlshRoot = File.join(File.dirname(__FILE__), '..', '..')
+
+  module_function
+
+  # Install a murlsh site to a web directory.
+  #
+  # Copies files that are different per-site to make a site instance.
+  def install(dest_dir)
+    Murlsh.cp_r_safe(
+      %w{
+        .htaccess
+        Rakefile
+        config.ru
+        config.yaml
+        plugins/
+        public/
+      }.map { |x| File.join(MurlshRoot, x) }, dest_dir, :verbose => true)
+
+    FileUtils.mkdir_p(File.join(dest_dir, 'tmp'), :verbose => true)
+  end
+
+end

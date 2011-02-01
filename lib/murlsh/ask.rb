ad0632b25bd87f63c2363519eba22a0760ad4304
Matthew M. Boedicker <matthewm@boedicker.org>
2011-01-31 22:20:30 -0500

    make default optional

--- lib/murlsh/ask.rb
+++ lib/murlsh/ask.rb
@@ -3,10 +3,12 @@ module Murlsh
   module_function
 
   # Ask the user a question and return the answer.
-  def ask(prompt, default)
-    print "#{prompt} [#{default}] "
+  def ask(prompt, default=nil)
+    default_given = !default.to_s.empty?
+    print "#{prompt} "
+    print "[#{default}] "  if default_given
     answer = $stdin.gets.strip
-    answer = default  if answer.empty?
+    answer = default  if answer.empty? and default_given
     answer
   end
 

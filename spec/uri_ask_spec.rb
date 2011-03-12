7409fb52c669243eb8cffe2e57af49685d9d7376
Matthew M. Boedicker <matthewm@boedicker.org>
2011-03-12 12:22:10 -0500

    use fake web instead of real web

--- spec/uri_ask_spec.rb
+++ spec/uri_ask_spec.rb
@@ -1,8 +1,42 @@
 require 'uri'
 
+require 'fakeweb'
+
 require 'murlsh'
 
 describe Murlsh::UriAsk do
+
+  before(:all) do
+    # FakeWeb.allow_net_connect = false
+
+    @url_invalid_host = 'http://x.boedicker.org/'
+
+    @url_404 = 'http://matthewm.boedicker.org/does_not_exist/'
+    [:get, :head].each do |method|
+      FakeWeb.register_uri(method, @url_404, :status => ['404', 'Not Found'])
+    end
+
+    @url_200 = 'http://matthemw.boedicker.org/good/'
+    FakeWeb.register_uri(:get, @url_200, :content_type => 'text/html',
+      :status => ['200', 'OK'],
+      :body => '<html><title>good</title><body></body></html>')
+    FakeWeb.register_uri(:head, @url_200, :content_type => 'text/html',
+      :status => ['200', 'OK'])
+
+    @url_200_https = 'https://matthemw.boedicker.org/good/'
+    FakeWeb.register_uri(:get, @url_200_https, :content_type => 'text/html',
+      :status => ['200', 'OK'],
+      :body => '<html><title>good</title><body></body></html>')
+    FakeWeb.register_uri(:head, @url_200_https, :content_type => 'text/html',
+      :status => ['200', 'OK'])
+
+    @url_203 = 'http://matthewm.boedicker.org/203.html'
+    [:get, :head].each do |method|
+      FakeWeb.register_uri(method, @url_203, :content_type => 'text/html',
+        :status => ['203', 'Non-Authoritative Information'])
+    end
+
+  end
 
   def asker(s)
     URI(s).extend(Murlsh::UriAsk)
@@ -19,39 +53,36 @@ describe Murlsh::UriAsk do
   end
 
   it 'should return an empty string for the content type of a URI with an invalid hostname' do
-    content_type('http://a.b/test/').should be_empty
+    content_type(@url_invalid_host).should be_empty
   end
 
   it 'should return an empty string for the content type of a URI with a nonexistent path' do
-    content_type(
-      'http://matthewm.boedicker.org/does_not_exist/').should be_empty
+    content_type(@url_404).should be_empty
   end
 
   it 'should return text/html for the content type of a valid URI that is text/html' do
-    content_type('http://www.google.com/').should match(/^text\/html/)
+    content_type(@url_200).should match(/^text\/html$/)
   end
 
   it 'should return text/html for the content type of a valid https URI that is text/html' do
-    content_type('https://msp.f-secure.com/web-test/common/test.html'
-      ).should match(/^text\/html/)
+    content_type(@url_200_https).should match(/^text\/html$/)
   end
 
   it 'should return text/html for the content type of a URI that returns HTTP 203' do
-    content_type('http://www.youtube.com/watch?v=Vxq9yj2pVWk'
-      ).should match(/^text\/html/)
+    content_type(@url_203).should match(/^text\/html$/)
   end
 
   it 'should return an empty string for the content type of an invalid URI when given failproof option true' do
-    content_type('http://x.boedicker.org/', :failproof => true).should be_empty
+    content_type(@url_invalid_host, :failproof => true).should be_empty
   end
 
   it 'should raise a SocketError when getting the content type of a URI with an invalid hostname when given failproof option false' do
-    lambda { content_type('http://x.boedicker.org/', :failproof => false)
+    lambda { content_type(@url_invalid_host, :failproof => false)
       }.should raise_error(SocketError)
   end
 
   it 'should raise an HTTPError when getting the content type of a URI with an invalid path when given failproof option false' do
-    lambda { content_type('http://boedicker.org/invalid', :failproof => false)
+    lambda { content_type(@url_404, :failproof => false)
       }.should raise_error(OpenURI::HTTPError)
   end
 
@@ -75,26 +106,24 @@ describe Murlsh::UriAsk do
   end
 
   it 'should return the URI as title for a URI with an invalid host' do
-    title('http://28fac7a1ac51976c90016509d97c89ba.edu/'
-      ).should == 'http://28fac7a1ac51976c90016509d97c89ba.edu/'
+    title(@url_invalid_host).should == @url_invalid_host
   end
 
   it 'should return the page title as title for a valid URI' do
-    title('http://www.google.com/').should == 'Google'
+    title(@url_200).should == 'good'
   end
 
   it 'should return the URI as title for an invalid URI when the failproof option is true' do
-    title('http://x.boedicker.org/', :failproof => true
-      ).should == 'http://x.boedicker.org/'
+    title(@url_invalid_host, :failproof => true).should == @url_invalid_host
   end
 
   it 'should raise a SocketError when trying to get the title of a URI with an invalid hostname when given failproof option false' do
-    lambda { title('http://x.boedicker.org/', :failproof => false)
+    lambda { title(@url_invalid_host, :failproof => false)
       }.should raise_error(SocketError)
   end
 
   it 'should raise an HTTPError when getting the title of a URI with an invalid path when given failproof option false' do
-    lambda { title('http://boedicker.org/invalid', :failproof => false)
+    lambda { title(@url_404, :failproof => false)
       }.should raise_error(OpenURI::HTTPError)
   end
 
